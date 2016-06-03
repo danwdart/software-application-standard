4c6a0b938a6d21767737ba0b5b5b2e4d475981ba
Dan <git@dandart.co.uk>
2016-06-03 02:30:43 +0100

    un relooking

--- relook/relook.c
+++ relook/relook.c
@@ -4,6 +4,7 @@
 #include <sys/stat.h>
 #include <string.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 typedef int (*orig_access_f_type)(const char *pathname, int mode);
 orig_access_f_type orig_access;
@@ -17,7 +18,7 @@ orig_stat_f_type orig_stat;
 typedef int (*orig_lstat_f_type)(const char *pathname, struct stat *buf);
 orig_lstat_f_type orig_lstat;
 
-char * strrep(char *str, char *o_s, char *n_s) 
+char * strrep(char *str, char *o_s, char *n_s)
 {
     char *newstr = NULL;
     char *c = NULL;
@@ -34,7 +35,7 @@ char * strrep(char *str, char *o_s, char *n_s)
         return NULL;
     }
 
-    strncpy(newstr, str, c-str);  
+    strncpy(newstr, str, c-str);
     sprintf(newstr+(c-str), "%s%s", n_s, c+strlen(o_s));
 
     return newstr;
@@ -42,13 +43,51 @@ char * strrep(char *str, char *o_s, char *n_s)
 
 int access(const char *pathname, int mode)
 {
-    char *newpath = (char*)pathname;       
+    struct stat sb;
+
+    if (-1 == orig_lstat("/proc/self/exe", &sb)) {
+       perror("lstat");
+       exit(EXIT_FAILURE);
+    }
+    printf("Allocating %i bytes\n", (int)sb.st_size + 1);
+    char *linkname = malloc(sb.st_size + 1);
+    if (NULL == linkname) {
+        fprintf(stderr, "insufficient memory\n");
+        exit(EXIT_FAILURE);
+    }
+    ssize_t r = readlink("/proc/self/exe", linkname, sb.st_size);
+    if (r == -1) {
+       perror("readlink");
+       exit(EXIT_FAILURE);
+    }
+    if (r > sb.st_size) {
+       fprintf(stderr, "symlink increased in size "
+                       "between lstat() and readlink()\n");
+       exit(EXIT_FAILURE);
+    }
+    linkname[r] = '\0';
+
+    int ret = orig_access(pathname, mode);
+    if (0 == ret) {
+        printf("Access: Found %s\n", pathname);
+        return ret;
+    }
+    printf("Access: Not found: %s\n", pathname);
+    char *newpath = (char*)pathname;
+
     printf("Accessing %s with mode %i\n", pathname, mode);
     if (NULL != strstr(pathname, ".so")) {
 	printf("Relocating library\n");
         newpath = strrep(newpath, "Binaries", "Libraries");
         printf("Now accessing %s\n", newpath);
     }
+    if (NULL != strstr(pathname, "/usr/share")) {
+	printf("Relocating shared resource\n");
+        newpath = strrep(newpath, "/usr/share", linkname);
+        printf("Now accessing %s\n", newpath);
+    }
+
+    free(linkname);
     return orig_access(newpath, mode);
 }
 
